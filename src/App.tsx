b89b610bf2ff0999433c85ae706f8d4daedb2218
hinajawaid <hinajawaid.h@gmail.com>
2025-05-12 01:59:56 +0200

    Delete function added

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,22 @@ function App() {
     },[])
     //add todo post, body needed
     function handleSend() {
-        axios.post("/api/todo" ,{
-            description: newTodo , status: "OPEN"  //this is bod body: {} using newTodo state here. newTodo (the text input from the user)
+        axios.post("/api/todo", {
+            description: newTodo, status: "OPEN"  //this is bod body: {} using newTodo state here. newTodo (the text input from the user)
         })
             .then((response) => setTodos([...todos, response.data])) // spread operator (...) to take the existing todos array and append the new todo that was returned in response.data
-                .catch(error => {
-                    console.error("There was an error adding the todo!", error); // handle error if post fails
+            .catch(error => {
+                console.error("There was an error adding the todo!", error); // handle error if post fails
+            });
+    }
+        //Delete by id
+        function handleDelete(id:string) {
+            axios.delete("/api/todo/"+id) //url
+                .then(()=>{
+                    setTodos(todos.filter(todo => todo.id !== id));
+                })
+                .catch((error) => {
+                    console.error("There was an error deleting the todo!", error); //catch error
                 });
         }
 
@@ -36,6 +46,8 @@ function App() {
                     <li key={todo.id}>
                         <h2>{todo.description}</h2>
                         <p>{todo.status}</p>
+                        {/* delete */}
+                        <button onClick={() => handleDelete(todo.id)}>Delete</button>
                     </li>
                 ))}
             </ul>
